1b2b04e623dea2b802b76dfb1ab0577a7ffffa6c
Allison  Craig <allison@craig.net>
2015-07-23 22:31:15 -0700

    first commit

---
+++ static/ajax-review.js
@@ -0,0 +1,35 @@
+// PART 1: SHOW A FORTUNE
+
+function showFortune(evt) {
+    // TODO: get the fortune and show it in the fortune-text div
+    $.get('/fortune', function(data) {
+    	$('#fortune-text').html(data)
+    })
+   
+}
+
+$('#get-fortune-button').on('click', showFortune);
+
+
+
+
+
+// PART 2: SHOW WEATHER
+
+function showWeather(evt) {
+    evt.preventDefault();
+    var url = "/weather?zipcode=" + $("#zipcode-field").val();
+    // TODO: request weather with that URL and show the forecast in #weather-info
+    $.get(url, function(data) {
+
+    
+    	console.log(data)
+    	console.log(url)
+
+    	$('#weather-info').html(data.forecast + "<br>" + data.temp);
+    })
+
+    
+}
+
+$("#weather-form").on('submit', showWeather);
